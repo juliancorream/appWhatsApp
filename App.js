b75b5044903d84b0e5d6b50f62a81036ab026dfc
Julian Correa <juliancorream@gmail.com>
2023-11-09 00:21:48 -0500

    InpuitBox Message Data Images OK

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import ChatListItem from "./src/components/ChatListItem";
-
-const chat = {
-  id: "1",
-  user: {
-    image: "https://hips.hearstapps.com/hmg-prod/images/homer-1658234509.jpeg",
-    name: "Julian Correa",
-  },
-  lastMessage: {
-    text: "Buenas Tardes como estan",
-    createdAt: "10:30",
-  },
-};
+import { StyleSheet, View } from "react-native";
+import ChatsScreen from "./src/screens/ChatsScreen";
+import ChatScreen from "./src/screens/ChatScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatListItem chat={chat} />
+      <ChatScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -27,7 +16,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    paddingVertical: 50,
   },
 });
